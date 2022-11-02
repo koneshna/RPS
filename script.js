c7b8632bf0d5cfb73f86f68d93a350ce5e15661b
koneshna <guditisn@gmail.com>
2022-11-02 15:59:21 -0400

    commented out js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let compChoice = {Value: ""};
-let playerChoice;
-function computerPlay(compChoice){
-    let choiceNum = Math.floor(Math.random() * 3)
-    if (choiceNum == 0){
-        compChoice.Value = 'rock';
-    }
-    else if (choiceNum == 1){
-        compChoice.Value = 'paper';
-    }
-    else if (choiceNum == 2){
-        compChoice.Value = 'scissors'
-    }
-    return choiceNum;
-    console.log(choiceNum)
-}
+// let compChoice = {Value: ""};
+// let playerChoice;
+// function computerPlay(compChoice){
+//     let choiceNum = Math.floor(Math.random() * 3)
+//     if (choiceNum == 0){
+//         compChoice.Value = 'rock';
+//     }
+//     else if (choiceNum == 1){
+//         compChoice.Value = 'paper';
+//     }
+//     else if (choiceNum == 2){
+//         compChoice.Value = 'scissors'
+//     }
+//     return choiceNum;
+//     console.log(choiceNum)
+// }
